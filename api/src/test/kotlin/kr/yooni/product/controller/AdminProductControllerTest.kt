abb3d87a60b36968334b588312bf334ae0eb5e31
yooni <dev.yooni@gmail.com>
2024-10-04 23:59:02 +0900

    fix

--- api/src/test/kotlin/kr/yooni/product/controller/AdminProductControllerTest.kt
+++ api/src/test/kotlin/kr/yooni/product/controller/AdminProductControllerTest.kt
@@ -15,8 +15,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(AdminProductController::class)
 class AdminProductControllerTest {
